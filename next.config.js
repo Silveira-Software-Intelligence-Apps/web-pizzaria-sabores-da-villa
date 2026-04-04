93498156e9052f2983f4affa70fa23173577be0f
Lead Machine <leadmachine@local>
2026-04-04 06:36:29 -0300

    feat: site gerado por Lead Machine

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['images.unsplash.com'] },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
 };
 module.exports = nextConfig;
